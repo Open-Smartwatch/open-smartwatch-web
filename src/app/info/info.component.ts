1dca70baf30e6d18a8769f7ea1ab8fa8e4bd3b82
simonmicro <simon@simonmicro.de>
2023-02-11 16:52:44 +0100

    Added some information about the UI itself

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiBuildFlag, ApiService } from '../api.service';
+import { VERSION } from '../../environments/version';
 
 @Component({
   selector: 'app-info',
@@ -7,15 +8,24 @@ import { ApiBuildFlag, ApiService } from '../api.service';
   styleUrls: ['./info.component.css']
 })
 export class InfoComponent {
-  public tags: ApiBuildFlag[] | undefined;
+  public tagsApi: ApiBuildFlag[] | undefined;
+  public tagsUI: ApiBuildFlag[] = [];
   public loading: boolean = true;
   public error: string = '';
 
   constructor(private apiService: ApiService) { }
 
   public async ngOnInit(): Promise<void> {
+    this.tagsUI.push({
+      name: 'Version',
+      value: VERSION.version
+    });
+    this.tagsUI.push({
+      name: 'Git Commit Hash',
+      value: VERSION.hash + (VERSION.dirty ? '-dirty' : '')
+    });
     try {
-      this.tags = await this.apiService.getBuildFlags();
+      this.tagsApi = await this.apiService.getBuildFlags();
     } catch (e) {
       this.error = `${e}`;
     }
