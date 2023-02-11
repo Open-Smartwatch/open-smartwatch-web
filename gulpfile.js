3faacd008fc1947332b225f7a9fae3463002d7ad
simonmicro <simon@simonmicro.de>
2023-02-11 16:23:30 +0100

    Removed any automatic compression

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var gzip = require('gulp-gzip');
-
-gulp.task('compress', function() {
-  return gulp.src(['./dist/**/*.*'])
-      .pipe(gzip({ gzipOptions: { level: 9 } }))
-      .pipe(gulp.dest('./dist.gz'));
-});
